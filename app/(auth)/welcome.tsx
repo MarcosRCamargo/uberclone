820eb1f9ea7da2e7ad82ad9286fd3c711d0964b3
Marcos R. Camargo <marcos.marrize@gmail.com>
2024-09-20 13:39:36 -0300

    Tela welcome

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,8 +1,13 @@
 import { router } from "expo-router";
-import { Text, TouchableOpacity } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Swiper from "react-native-swiper";
+import { useRef, useState } from "react";
+import { onboarding } from "../../app/constants";
 
 const Onboarding = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
   return (
     <SafeAreaView className="flex h-full items-center  justify-between bg-white">
       <TouchableOpacity
@@ -13,6 +18,35 @@ const Onboarding = () => {
       >
         <Text className="text-black text-md">Skip</Text>
       </TouchableOpacity>
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={
+          <View className="w-[32px] h[4px] mx-1 bg-[#E2e8f0] rounded-full" />
+        }
+        activeDot={
+          <View className="w-[32px] h[4px] mx-1 bg-[#0280FF] rounded-full" />
+        }
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
+        {onboarding.map((item) => (
+          <View className="flex flex-col items-center justify-center">
+            <View className="w-[200px] h-[200px] rounded-full overflow-hidden">
+              <Image
+                source={{ uri: item.image }}
+                className="w-full h-full"
+                resizeMode="contain"
+              />
+            </View>
+            <Text className="text-2xl font-bold text-black mt-5">
+              {item.title}
+            </Text>
+            <Text className="text-md text-center text-[#4B5563] px-10 mt-3">
+              {item.description}
+            </Text>
+          </View>
+        ))}
+      </Swiper>
     </SafeAreaView>
   );
 };
---
+++ app/constants/index.tsx
@@ -0,0 +1,8 @@
+export const onboarding = [
+  {
+    id: 1,
+    title: "Welcome to the app",
+    description: "This is a description of the app",
+    image: "https://via.placeholder.com/150",
+  },
+];
