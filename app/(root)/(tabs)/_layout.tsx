a745b8de45d00bfa632f4d99d81f456450bf4b15
Marcos R. Camargo <marcos.marrize@gmail.com>
2024-09-20 11:08:00 -0300

    Correção das rotas

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -3,8 +3,8 @@ import { Stack } from "expo-router";
 const Tabs = () => {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="home" options={{ headerShown: false }} />
+      <Stack.Screen name="chat" options={{ headerShown: false }} />
       <Stack.Screen name="profile" options={{ headerShown: false }} />
       <Stack.Screen name="rides" options={{ headerShown: false }} />
     </Stack>
